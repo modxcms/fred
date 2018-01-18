b244c6fc413612526f95498fd6c12d0a30a6cd76
Jan Peca <pecajan@gmail.com>
2018-01-18 20:29:05 +0100

    Divide to several classes

---
+++ src/Topbar.js
@@ -0,0 +1,45 @@
+export default class Topbar {
+    constructor(wrapper) {
+        wrapper.appendChild(this.buildTopBar());
+        
+        return this;
+    }
+    
+    liWrapper(node) {
+        const li = document.createElement('li');
+        li.innerHTML = node.outerHTML;
+
+        return li;
+    }
+
+    buildTopBar() {
+        const topBar = document.createElement('div');
+        topBar.classList.add('fred--topbar', 'fred--clearfix');
+
+        const links = document.createElement('ul');
+        links.classList.add('fred--topbar_links');
+
+        const newPage = document.createElement('a');
+        newPage.innerHTML = 'New Page <i class="fa fa-angle-down"></i>';
+
+        const settings = document.createElement('a');
+        settings.innerHTML = 'Settings <i class="fa fa-angle-down"></i>';
+
+        links.appendChild(this.liWrapper(newPage));
+        links.appendChild(this.liWrapper(settings));
+
+        const buttons = document.createElement('div');
+        buttons.classList.add('fred--topbar_buttons');
+
+        const logout = document.createElement('button');
+        logout.classList.add('fred--btn-small');
+        logout.innerText = 'Logout';
+
+        buttons.appendChild(logout);
+
+        topBar.appendChild(links);
+        topBar.appendChild(buttons);
+
+        return topBar;
+    }
+}
