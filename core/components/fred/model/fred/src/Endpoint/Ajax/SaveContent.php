a5274ad9580c624f16d6101ad9a5f6f8becce3b1
Jason Coward <jason@opengeek.com>
2018-02-12 16:02:20 -0700

    Save more resource data

--- core/components/fred/model/fred/src/Endpoint/Ajax/SaveContent.php
+++ core/components/fred/model/fred/src/Endpoint/Ajax/SaveContent.php
@@ -21,11 +21,47 @@ class SaveContent extends Endpoint
             return $this->failure('Could not load resource with id ' . (int)$this->body['id']);
         }
 
+        if (!$object->checkPolicy('save')) {
+            return $this->failure('Permission denied (save)');
+        }
+
+        if (isset($this->body['content'])) {
+            $object->setContent($this->body['content']);
+        }
+
+        if (isset($this->body['introtext'])) {
+            $object->set('introtext', $this->body['introtext']);
+        }
+
+        if (isset($this->body['pagetitle'])) {
+            $object->set('pagetitle', $this->body['pagetitle']);
+        }
+
+        if (isset($this->body['longtitle'])) {
+            $object->set('longtitle', $this->body['longtitle']);
+        }
+
+        if (isset($this->body['published'])) {
+            if (!$object->checkPolicy('publish')) {
+                return $this->failure('Permission denined (publish)');
+            }
+
+            $currentValue = (boolean)$object->get('published');
+            $incomingValue = (boolean)$this->body['published'];
+            if ($incomingValue !== $currentValue) {
+                $object->set('published', (boolean)$this->body['published']);
+                $object->set('publishedon', $currentValue ? time() : false);
+            }
+        }
+
         $object->setProperty('data', $this->body['data'], 'fred');
+        $object->set('editedon', time());
+        $object->set('editedby', $this->modx->user->get('id'));
+
         $saved = $object->save();
 
         if (!$saved) {
-            return $this->failure('Error saving fred properties to resource with id ' . $object->get('id'));
+            return $this->failure('Error saving resource with id ' . $object->get('id'));
         }
 
         return $this->success('Save successful');
