a0ca7a0c3b8655ca7c3e6294d406aa62e71da90d
matdave <mat@matdave.com>
2018-02-13 11:11:27 -0600

    making nav sticky

--- _build/assets/js/Components/Sidebar.js
+++ _build/assets/js/Components/Sidebar.js
@@ -11,7 +11,7 @@ export default class Sidebar {
             const dt = document.createElement('dt');
             dt.setAttribute('role', 'tab');
             dt.setAttribute('tabindex', '0');
-            dt.innerHTML = ((expandable === true) ? text + '<i class="fred--angle-right fred--accordion_toggle"></i>' : '') + '</span>';
+            dt.innerHTML = text + ((expandable === true) ? '<i class="fred--angle-right fred--accordion_toggle"></i>' : '');
 
             if (expandable === false) {
                 dt.addEventListener('click', this.click);
