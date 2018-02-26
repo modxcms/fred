c247245566d9f693bf65f952ad92aba26f572503
Jan Peca <pecajan@gmail.com>
2018-02-26 12:25:15 +0100

    Replace init snippet with a plugin

--- core/components/fred/lexicon/en/default.inc.php
+++ core/components/fred/lexicon/en/default.inc.php
@@ -10,3 +10,8 @@ $_lang['fred'] = 'Fred';
 
 $_lang['setting_fred.launcher_position'] = 'Position of Launcher';
 $_lang['setting_fred.launcher_position_desc'] = 'Available values: bottom_left, bottom_right, top_left, top_right';
+$_lang['setting_fred.elements_category_id'] = 'Element\'s Category ID';
+$_lang['setting_fred.elements_category_id_desc'] = 'ID of Category to use as an element source.';
+$_lang['setting_fred.template_ids'] = 'Template IDs';
+$_lang['setting_fred.template_ids_desc'] = 'Comma separated list of template IDs where Fred will init.';
+
