5200c770de9b2ad1cd684eb451012d36b690066d
matdave <mat@matdave.com>
2018-04-06 15:22:48 -0500

    Updated plugin to block content area

--- core/components/fred/elements/plugins/Fred.php
+++ core/components/fred/elements/plugins/Fred.php
@@ -1,38 +1,73 @@
 <?php
 $templates = $modx->getOption('fred.template_ids');
 $templates = explode(',', $templates);
-$templates = array_map('trim', $templates);
-$templates = array_flip($templates);
-
-if (isset($templates[$modx->resource->template])) {
-    if (isset($_GET['fred'])) {
-        if (intval($_GET['fred']) === 0) return;
-    }
-
-    $corePath = $modx->getOption('fred.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/fred/');
-    /** @var Fred $fred */
-    $fred = $modx->getService(
-        'fred',
-        'Fred',
-        $corePath . 'model/fred/',
-        array(
-            'core_path' => $corePath
-        )
-    );
-
-    $fredContent = '
-    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.9/tinymce.min.js"></script>
-    <script type="text/javascript" src="'.$fred->getOption('webAssetsUrl') . 'fred.min.js"></script>
-    <link rel="stylesheet" href="'.$fred->getOption('webAssetsUrl') . 'fred.css" type="text/css" />
-    <script>
-        var fred = new Fred({
-            assetsUrl: "' . $fred->getOption('webAssetsUrl') . '",
-            launcherPosition: "' . $fred->getOption('launcher_position') . '",
-            resource: {
-                "id": ' . $modx->resource->id . '
+switch ($modx->event->name) {
+    case 'OnDocFormPrerender':
+        $templates =  array_map('intval', $templates);
+        if(!empty($modx->resource) && in_array($modx->resource->template,$templates)){
+            $modx->lexicon->load('fred:default');
+            $modx->controller->addHtml("
+        <script>
+            Ext.ComponentMgr.onAvailable('modx-resource-content', function(right) {
+                right.on('beforerender', function() {
+                    var content = Ext.getCmp('ta'),
+                    panel = Ext.getCmp('modx-page-update-resource'); 
+                    
+                    content.destroy();
+                     
+                    right.insert(0,{
+                        xtype: 'button' 
+                        ,fieldLabel: 'View in Fred'
+                        ,hideLabel: true
+                        ,cls: 'primary-button'
+                        ,style: {padding: '10px 15px'}
+                        ,html: '".$modx->lexicon('resource_fred.open')."'
+                        ,handler: function(){
+                            window.open(panel.config.preview_url)
+                        }
+                    });
+                })
+            });
+            
+        </script>");
+        }
+        break;
+    case 'OnWebPagePrerender':
+        $templates = array_map('trim', $templates);
+        $templates = array_flip($templates);
+        if (isset($templates[$modx->resource->template])) {
+            if (isset($_GET['fred'])) {
+                if (intval($_GET['fred']) === 0) return;
             }
-        });
-    </script>';
 
-    $modx->resource->_output = preg_replace('/(<\/head>(?:<\/head>)?)/i',"{$fredContent}\r\n$1", $modx->resource->_output);
+            $corePath = $modx->getOption('fred.core_path', null, $modx->getOption('core_path', null, MODX_CORE_PATH) . 'components/fred/');
+            /** @var Fred $fred */
+            $fred = $modx->getService(
+                'fred',
+                'Fred',
+                $corePath . 'model/fred/',
+                array(
+                    'core_path' => $corePath
+                )
+            );
+
+            $fredContent = '
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tinymce/4.7.9/tinymce.min.js"></script>
+        <script type="text/javascript" src="' . $fred->getOption('webAssetsUrl') . 'fred.min.js"></script>
+        <link rel="stylesheet" href="' . $fred->getOption('webAssetsUrl') . 'fred.css" type="text/css" />
+        <script>
+            var fred = new Fred({
+                assetsUrl: "' . $fred->getOption('webAssetsUrl') . '",
+                launcherPosition: "' . $fred->getOption('launcher_position') . '",
+                resource: {
+                    "id": ' . $modx->resource->id . '
+                }
+            });
+        </script>';
+
+            $modx->resource->_output = preg_replace('/(<\/head>(?:<\/head>)?)/i', "{$fredContent}\r\n$1", $modx->resource->_output);
+        }
+        break;
 }
+
+return;
