6d593ee6bbf8c709e60331f6c4ce16a2bff41215
matdave <mat@matdave.com>
2018-06-27 15:44:06 -0500

    Add setting for default element ID|target

--- core/components/fred/lexicon/en/default.inc.php
+++ core/components/fred/lexicon/en/default.inc.php
@@ -15,6 +15,15 @@ $_lang['setting_fred.launcher_position'] = 'Position of Launcher';
 $_lang['setting_fred.launcher_position_desc'] = 'Available values: bottom_left, bottom_right, top_left, top_right';
 $_lang['setting_fred.elements_category_id'] = 'Element\'s Category ID';
 $_lang['setting_fred.elements_category_id_desc'] = 'ID of Category to use as an element source.';
+$_lang['setting_fred.element_group_sort'] = 'Element\'s Group Sort';
+$_lang['setting_fred.element_group_sort_desc'] = 'Sort elements by name or rank';
+$_lang['setting_fred.icon_editor'] = 'Icon Editor';
+$_lang['setting_fred.icon_editor_desc'] = 'Editor to use for Icons';
+$_lang['setting_fred.image_editor'] = 'Image Editor';
+$_lang['setting_fred.image_editor_desc'] = 'Editor to use for Images';
+$_lang['setting_fred.rte'] = 'Rich Text Editor';
+$_lang['setting_fred.rte_desc'] = 'Editor to use for Rich Text areas';
 $_lang['setting_fred.template_ids'] = 'Template IDs';
 $_lang['setting_fred.template_ids_desc'] = 'Comma separated list of template IDs where Fred will init.';
-
+$_lang['setting_fred.default_element'] = 'Default Element';
+$_lang['setting_fred.default_element_desc'] = 'Set a default <b>element ID</b> and <b>target</b> for converting resources to Fred. E.g. 10|target';
