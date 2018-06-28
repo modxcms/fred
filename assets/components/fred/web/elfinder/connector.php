429bb17d438466f6b6e281b7a49d030033ece169
Jan Peca <pecajan@gmail.com>
2018-06-28 11:06:32 +0200

    Add copyright header to all php files

--- assets/components/fred/web/elfinder/connector.php
+++ assets/components/fred/web/elfinder/connector.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ * This file is part of the Fred package.
+ *
+ * Copyright (c) MODX, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 require_once dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . '/config.core.php';
 require_once MODX_CORE_PATH.'model/modx/modx.class.php';
