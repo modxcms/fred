969124cac27c44d5aebb130f4dbe473d3756676c
Jan Peca <pecajan@gmail.com>
2018-06-28 11:06:32 +0200

    Add copyright header to all php files

--- core/components/fred/controllers/home.class.php
+++ core/components/fred/controllers/home.class.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of the Fred package.
+ *
+ * Copyright (c) MODX, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 require_once dirname(dirname(__FILE__)) . '/index.class.php';
 
 /**
--- core/components/fred/model/fred/src/Endpoint/Endpoint.php
+++ core/components/fred/model/fred/src/Endpoint/Endpoint.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of the Fred package.
+ *
+ * Copyright (c) MODX, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Fred\Endpoint;
 
 abstract class Endpoint
