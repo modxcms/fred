4d441dad2e578e56ed02cde6cc797ef7bd3d8d1e
Ivan Bochkarev <ivanx86@gmail.com>
2018-07-09 00:33:27 +0600

    Add ru lexicon

---
+++ core/components/fred/lexicon/ru/fe.inc.php
@@ -0,0 +1,97 @@
+<?php
+/*
+ * This file is part of the Fred package.
+ *
+ * Copyright (c) MODX, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Default English Lexicon Entries for Fred
+ *
+ * @package fred
+ * @subpackage lexicon
+ */
+
+$_lang['fred.fe.close'] = 'Закрыть';
+$_lang['fred.fe.save'] = 'Сохранить';
+$_lang['fred.fe.pages'] = 'Сайт';
+$_lang['fred.fe.elements'] = 'Элементы';
+$_lang['fred.fe.page_settings'] = 'Настройки';
+$_lang['fred.fe.more'] = 'Другое';
+$_lang['fred.fe.saving_page'] = 'Сохранить страницу';
+$_lang['fred.fe.preparing_content'] = 'Подготовка контента';
+$_lang['fred.fe.open_sidebar'] = 'Открыть панель';
+$_lang['fred.fe.toggle_preview'] = 'Предварительный просмотр';
+$_lang['fred.fe.close_sidebar'] = 'Закрыть панель';
+$_lang['fred.fe.element_settings'] = 'Настройка элементов';
+$_lang['fred.fe.apply'] = 'Применить';
+$_lang['fred.fe.cancel'] = 'Отмена';
+$_lang['fred.fe.clear'] = 'Очистить';
+$_lang['fred.fe.browse'] = 'Обзор';
+$_lang['fred.fe.browse_files'] = 'Обзор файлов';
+$_lang['fred.fe.browse_images'] = 'Обзор изображений';
+
+$_lang['fred.fe.page_settings.page_title'] = 'Заголовок страницы';
+$_lang['fred.fe.page_settings.long_title'] = 'Длинное название';
+$_lang['fred.fe.page_settings.description'] = 'Описание';
+$_lang['fred.fe.page_settings.intro_text'] = 'Вводный текст';
+$_lang['fred.fe.page_settings.menu_title'] = 'Название меню';
+$_lang['fred.fe.page_settings.alias'] = 'Псевдоним';
+$_lang['fred.fe.page_settings.published'] = 'Опубликован';
+$_lang['fred.fe.page_settings.hide_from_menu'] = 'Не показывать в меню';
+$_lang['fred.fe.page_settings.advanced_settings'] = 'Дополнительные настройки';
+$_lang['fred.fe.page_settings.published_on'] = 'Опубликован в';
+$_lang['fred.fe.page_settings.publish_on'] = 'Опубликовать в';
+$_lang['fred.fe.page_settings.unpublish_on'] = 'Снять с публикации';
+$_lang['fred.fe.page_settings.menu_index'] = 'Пункт меню';
+$_lang['fred.fe.page_settings.deleted'] = 'Удалён';
+
+$_lang['fred.fe.pages.no_parent'] = 'Нет родителя';
+$_lang['fred.fe.pages.create_page'] = 'Создать страницу';
+$_lang['fred.fe.pages.parent'] = 'Родительский ресурс';
+$_lang['fred.fe.pages.template'] = 'Шаблон';
+$_lang['fred.fe.pages.page_title'] = 'Заголовок страницы';
+$_lang['fred.fe.pages.creating_page'] = 'Создание страницы';
+$_lang['fred.fe.pages.expand_page'] = 'Развернуть страницу';
+$_lang['fred.fe.pages.collapse_page'] = 'Свернуть страницу';
+$_lang['fred.fe.pages.edit'] = 'Редактировать';
+$_lang['fred.fe.pages.duplicate'] = 'Дублировать';
+$_lang['fred.fe.pages.publish'] = 'Публиковать';
+$_lang['fred.fe.pages.unpublish'] = 'Снять с публикации';
+$_lang['fred.fe.pages.create_child_page'] = 'Создать дочернюю страницу';
+$_lang['fred.fe.pages.delete'] = 'Удалить';
+$_lang['fred.fe.pages.undelete'] = 'Восстановить';
+
+$_lang['fred.fe.more.openmanager'] = 'Просмотреть в менеджере';
+$_lang['fred.fe.more.logout'] = 'Выйти';
+
+$_lang['fred.fe.content.duplicate'] = 'Дубликат';
+$_lang['fred.fe.content.delete'] = 'Удалить';
+$_lang['fred.fe.content.settings'] = 'Открыть настройки';
+$_lang['fred.fe.content.move_up'] = 'Переместить вверх';
+$_lang['fred.fe.content.move_down'] = 'Переместить вниз';
+
+$_lang['fred.fe.element_settings.unsaved_changes'] = 'Возможно, внесенные изменения не сохранятся.';
+
+$_lang['fred.fe.editor.edit'] = 'Редактировать';
+$_lang['fred.fe.editor.edit_icon'] = 'Редактировать иконку';
+$_lang['fred.fe.editor.edit_image'] = 'Редактировать изображение';
+$_lang['fred.fe.editor.image_uri'] = 'URI изображения';
+
+$_lang['fred.fe.preview.tablet_portrait'] = 'Планшет (портрет)';
+$_lang['fred.fe.preview.tablet_landscape'] = 'Планшет (ландшафт)';
+$_lang['fred.fe.preview.phone_portrait'] = 'Телефон (портрет)';
+$_lang['fred.fe.preview.phone_landscape'] = 'Смартфон (ландшафт)';
+$_lang['fred.fe.preview.auto'] = 'Авто';
+
+$_lang['fred.fe.tagger.tagger'] = 'Теги';
+$_lang['fred.fe.tagger.add'] = 'Добавить тег';
+$_lang['fred.fe.tagger.toggle_list'] = 'Переключить список';
+$_lang['fred.fe.tagger.toggle_input'] = 'Переключить ввод';
+
+$_lang['fred.fe.permission.save'] = 'Недостаточно прав для сохранения этого ресурса!';
+$_lang['fred.fe.permission.new_document'] = 'Недостаточно прав для создания ресурса!';
+$_lang['fred.fe.permission.new_document_in_root'] = 'Недостаточно прав для создания ресурса в корне!';
