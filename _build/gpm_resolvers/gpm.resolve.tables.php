5f39224a9b4ae069e1b3b2292b30516fd63ee201
matdave <mat@matdave.com>
2018-07-20 11:46:14 -0500

    Packaged beta3

---
+++ _build/gpm_resolvers/gpm.resolve.tables.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Resolve creating db tables
+ *
+ * THIS RESOLVER IS AUTOMATICALLY GENERATED, NO CHANGES WILL APPLY
+ *
+ * @package fred
+ * @subpackage build
+ *
+ * @var mixed $object
+ * @var modX $modx
+ * @var array $options
+ */
+
+if ($object->xpdo) {
+    $modx =& $object->xpdo;
+    switch ($options[xPDOTransport::PACKAGE_ACTION]) {
+        case xPDOTransport::ACTION_INSTALL:
+        case xPDOTransport::ACTION_UPGRADE:
+            $modelPath = $modx->getOption('fred.core_path', null, $modx->getOption('core_path') . 'components/fred/') . 'model/';
+            
+            $modx->addPackage('fred', $modelPath, null);
+
+
+
+            $manager = $modx->getManager();
+
+            $manager->createObjectContainer('FredBlueprintCategory');
+            $manager->createObjectContainer('FredBlueprint');
+
+
+            break;
+    }
+}
+
+return true;
