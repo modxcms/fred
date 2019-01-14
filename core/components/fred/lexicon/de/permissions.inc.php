2fab34d72f264f2f111721c2226c178df175e56a
bYemma <kontakt@byemma.de>
2019-01-14 11:46:33 +0100

    Updated german translation

---
+++ core/components/fred/lexicon/de/permissions.inc.php
@@ -0,0 +1,60 @@
+<?php
+/*
+ * This file is part of the Fred package.
+ *
+ * Copyright (c) MODX, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+/**
+ * Permission German Lexicon Entries for Fred
+ *
+ * @package fred
+ * @subpackage lexicon
+ */
+$_lang['fred.permissions.fred'] = 'Fred ansehen (mgr) (web)';
+$_lang['fred.permissions.fred_elements'] = 'Elemente ansehen (mgr) (web)';
+$_lang['fred.permissions.fred_element_save'] = 'Elemente anlegen/bearbeiten/duplizieren (mgr)';
+$_lang['fred.permissions.fred_element_delete'] = 'Elemente löschen (mgr)';
+$_lang['fred.permissions.fred_element_screenshot'] = 'Screenshot erstellen, um die Seitenleisten Vorschau für Elemente zu ersetzen (web)';
+$_lang['fred.permissions.fred_element_move'] = 'Elemente in die Dropzones ziehen (web)';
+$_lang['fred.permissions.fred_element_category_delete'] = 'Element Kategorien löschen (mgr)';
+$_lang['fred.permissions.fred_element_category_save'] = 'Element Kategorien anlegen/bearbeiten/duplizieren (mgr)';
+$_lang['fred.permissions.fred_element_categories'] = 'Fred Element Kategorien ansehen (mgr)';
+$_lang['fred.permissions.fred_element_option_sets_save'] = 'Einstellungs-Gruppen anlegen/bearbeiten/duplizieren (mgr)';
+$_lang['fred.permissions.fred_element_option_sets_delete'] = 'Einstellungs-Gruppen löschen (mgr)';
+$_lang['fred.permissions.fred_element_option_sets'] = 'Einstellungs-Gruppen ansehen (mgr)';
+$_lang['fred.permissions.fred_element_rte_config_save'] = 'RTE Konfigurationen anlegen/bearbeiten/duplizieren (mgr)';
+$_lang['fred.permissions.fred_element_rte_config_delete'] = 'RTE Konfigurationen löschen (mgr)';
+$_lang['fred.permissions.fred_element_rtes'] = 'RTE Konfigurationen ansehen (mgr)';
+$_lang['fred.permissions.fred_element_rebuild'] = 'Rebuild Tab ansehen (mgr)';
+$_lang['fred.permissions.fred_themes'] = 'Themes Tab ansehen (mgr)';
+$_lang['fred.permissions.fred_themes_save'] = 'Themes anlegen/bearbeiten/duplizieren (mgr)';
+$_lang['fred.permissions.fred_themes_delete'] = 'Themes löschen (mgr)';
+$_lang['fred.permissions.fred_themes_build'] = 'Themes packen (mgr)';
+$_lang['fred.permissions.fred_themed_templates_save'] = 'Fred Themes MODX Templates zuweisen/updaten (mgr)';
+$_lang['fred.permissions.fred_themed_templates_delete'] = 'Theme von MODX Template entfernen (mgr)';
+$_lang['fred.permissions.fred_themed_templates'] = 'Zuweisungen Theme/Template ansehen (mgr)';
+$_lang['fred.permissions.fred_settings'] = 'Einstellungsmenü in der Seitenleiste ansehen (web)';
+$_lang['fred.permissions.fred_settings_tvs'] = 'TV Abschnitt unter dem Einstellungsmenü ansehen (web)';
+$_lang['fred.permissions.fred_settings_tags'] = 'Tag Abschnitt unter dem Einstellungsmenü ansehen (web)';
+$_lang['fred.permissions.fred_settings_advanced'] = 'Erweiterte Einstellungen unter dem Einstellungsmenü ansehen (web)';
+$_lang['fred.permissions.fred_media_sources'] = 'Medienquellen ansehen (mgr)';
+$_lang['fred.permissions.fred_blueprints'] = 'Blaupausen ansehen (mgr) (web)';
+$_lang['fred.permissions.fred_blueprints_save'] = 'Blaupausen anlegen/bearbeiten/duplizieren (mgr) (web)';
+$_lang['fred.permissions.fred_blueprints_delete'] = 'Blaupausen löschen (mgr)';
+$_lang['fred.permissions.fred_blueprints_create_public'] = 'Öffentliche Blaupausen anlegen (mgr) (web)';
+$_lang['fred.permissions.fred_blueprint_categories'] = 'Blaupausen Kategorien ansehen (mgr)';
+$_lang['fred.permissions.fred_blueprint_categories_save'] = 'Blaupausen Kategorien anlegen/bearbeiten/duplizieren (mgr) (web)';
+$_lang['fred.permissions.fred_blueprint_categories_delete'] = 'Blaupausen Kategorien löschen (mgr)';
+$_lang['fred.permissions.fred_blueprint_categories_create_public'] = 'Öffentliche Blaupausen Kategorien anlegen (mgr) (web)';
+$_lang['fred.permissions.new_document_in_root'] = 'Ressourcen in der obersten Ebene des Ressourcenbaums anlegen (mgr) (web)';
+$_lang['fred.permissions.view_unpublished'] = 'Unveröffentlichte Ressourcen ansehen (web)';
+$_lang['fred.permissions.resource_duplicate'] = 'Ressourcen duplizieren (mgr) (web)';
+$_lang['fred.permissions.unpublish_document'] = 'Ressourcen zurückziehen (mgr) (web)';
+$_lang['fred.permissions.publish_document'] = 'Ressourcen veröffentlichen (mgr) (web)';
+$_lang['fred.permissions.save_document'] = 'Ressourcen speichern (mgr) (web)';
+$_lang['fred.permissions.new_document'] = 'Neue Ressourcen anlegen (mgr) (web)';
+$_lang['fred.permissions.delete_document'] = 'Ressourcen löschen (mgr) (web)';
+$_lang['fred.permissions.undelete_document'] = 'Ressourcen wiederherstellen (mgr) (web)';
