40668a42eb2ccb27be422282d53cb5c9a27f8627
Jan Peca <pecajan@gmail.com>
2019-01-25 11:54:45 +0100

    Fix the search for page input
    Resolves #110

--- core/components/fred/model/fred/src/Endpoint/Ajax/GetResources.php
+++ core/components/fred/model/fred/src/Endpoint/Ajax/GetResources.php
@@ -24,7 +24,8 @@ class GetResources extends Endpoint
      */
     function process()
     {
-        $context = 'web';
+        $context = $this->getClaim('context');
+        $context = !empty($context) ? $context : 'web';
         
         $query = $_GET['query'];
         $current = isset($_GET['current']) ? intval($_GET['current']) : 0;
