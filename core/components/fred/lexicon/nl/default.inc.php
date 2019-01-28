1e74902af6c0f604ddb5eeb27822d8faaab9eacf
Joeke Kloosterman <joeke@sterc.nl>
2019-01-28 11:00:21 +0100

    [in progress] First NL lexicons for Fred

---
+++ core/components/fred/lexicon/nl/default.inc.php
@@ -0,0 +1,279 @@
+<?php
+/*
+ * This file is part of the Fred package.
+ *
+ * Copyright (c) MODX, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Default English Lexicon Entries for Fred
+ *
+ * @package fred
+ * @subpackage lexicon
+ */
+
+$_lang['fred'] = 'Fred';
+$_lang['freddropzone'] = 'Fred Dropzone';
+
+$_lang['fred.global.any'] = 'Elke';
+$_lang['fred.global.change_order'] = 'Volgorde wijzigen: [[+name]]';
+$_lang['fred.global.help'] = 'Help';
+
+$_lang['fred.open_in_fred'] = 'Openen in Fred';
+
+$_lang['fred.menu.fred'] = 'Fred';
+$_lang['fred.menu.fred_desc'] = 'Elementen, blauwdrukken, themas en meer!';
+
+$_lang['fred.refresh_fail_resource'] = 'Geen Fred pagina\'s gevonden om te verversen';
+$_lang['fred.refresh_fail_template'] = 'Geen Fred templates gevonden om te verversen';
+$_lang['fred.refresh_complete'] = 'Bouwen klaar';
+$_lang['fred.refresh_id'] = 'Verversen pagina ID [[+id]]';
+
+$_lang['fred.home.page_title'] = 'Fred';
+$_lang['fred.home.element_categories'] = 'Element categorieën';
+$_lang['fred.home.elements'] = 'Elementen';
+$_lang['fred.home.option_sets'] = 'Optie Sets';
+$_lang['fred.home.rte_configs'] = 'RTE configuraties';
+$_lang['fred.home.blueprints'] = 'Blauwdrukken';
+$_lang['fred.home.blueprint_categories'] = 'Blauwdruk categorieën';
+$_lang['fred.home.themes'] = 'Themas';
+$_lang['fred.home.themed_templates'] = 'Thema templates';
+$_lang['fred.home.rebuild'] = 'Bouwen';
+$_lang['fred.home.media_sources'] = 'Media bronnen';
+
+$_lang['fred.blueprints.none'] = 'Geen blauwdrukken gevonden';
+$_lang['fred.blueprints.name'] = 'Naam';
+$_lang['fred.blueprints.description'] = 'Omschrijving';
+$_lang['fred.blueprints.complete'] = 'Compleet';
+$_lang['fred.blueprints.public'] = 'Publiek';
+$_lang['fred.blueprints.rank'] = 'Rang';
+$_lang['fred.blueprints.category'] = 'Categorie';
+$_lang['fred.blueprints.image'] = 'Afbeelding';
+$_lang['fred.blueprints.created_by'] = 'Aangemaakt door';
+$_lang['fred.blueprints.category'] = 'Categorie';
+$_lang['fred.blueprints.search_name'] = 'Zoeken op naam';
+$_lang['fred.blueprints.remove'] = 'Blauwdruk verwijderen';
+$_lang['fred.blueprints.remove_confirm'] = 'Weet je zeker dat je blauwdruk "[[+name]]" wilt verwijderen?';
+$_lang['fred.blueprints.quick_update'] = 'Blauwdruk bewerken';
+$_lang['fred.blueprints.update'] = 'Blauwdruk bewerken';
+$_lang['fred.blueprints.create'] = 'Nieuwe Blauwdruk';
+$_lang['fred.blueprints.theme'] = 'Thema';
+
+$_lang['fred.blueprint_cateogries.all'] = 'Alle categorieën';
+$_lang['fred.blueprint_categories.none'] = 'Geen categorieën gevonden.';
+$_lang['fred.blueprint_categories.name'] = 'Naam';
+$_lang['fred.blueprint_categories.public'] = 'Publiek';
+$_lang['fred.blueprint_categories.rank'] = 'Rang';
+$_lang['fred.blueprint_categories.created_by'] = 'Aangemaakt door';
+$_lang['fred.blueprint_categories.search_name'] = 'Zoeken op naam';
+$_lang['fred.blueprint_categories.create'] = 'Nieuwe categorie';
+$_lang['fred.blueprint_categories.update'] = 'Categorie bewerken';
+$_lang['fred.blueprint_categories.remove'] = 'Categorie verwijderen';
+$_lang['fred.blueprint_categories.remove_confirm'] = 'Weet je zeker dat je categorie "[[+name]]" [[+blueprints]] gekoppelde blauwdrukken wilt verwijderen?' ;
+$_lang['fred.blueprint_categories.remove_confirm_singular'] = 'Weet je zeker dat je categorie "[[+name]]" [[+blueprints]] gekoppelde blauwdruk wilt verwijderen?' ;
+$_lang['fred.blueprint_categories.remove_confirm_empty'] = 'Weet je zeker dat je categorie "[[+name]]" wilt verwijderen?' ;
+$_lang['fred.blueprint_categories.number_of_blueprints'] = '# Blauwdrukken';
+$_lang['fred.blueprint_categories.theme'] = 'Thema';
+
+$_lang['fred.elements.none'] = 'No elements were found.';
+$_lang['fred.elements.image'] = 'Image';
+$_lang['fred.elements.name'] = 'Name';
+$_lang['fred.elements.description'] = 'Description';
+$_lang['fred.elements.category'] = 'Category';
+$_lang['fred.elements.rank'] = 'Rank';
+$_lang['fred.elements.search_name'] = 'Search by Name';
+$_lang['fred.elements.remove'] = 'Remove Element';
+$_lang['fred.elements.remove_confirm'] = 'Are you sure you want to delete [[+name]] Element?';
+$_lang['fred.elements.quick_update'] = 'Quick Update Element';
+$_lang['fred.elements.update'] = 'Update Element';
+$_lang['fred.elements.create'] = 'Create Element';
+$_lang['fred.elements.markup'] = 'Element\'s Markup';
+$_lang['fred.elements.options'] = 'Options';
+$_lang['fred.elements.options_override'] = 'Options Override';
+$_lang['fred.elements.option_set'] = 'Option Set';
+$_lang['fred.elements.duplicate'] = 'Duplicate Element';
+$_lang['fred.elements.new_name'] = 'New Name';
+$_lang['fred.elements.has_override'] = 'Has Override';
+$_lang['fred.elements.theme'] = 'Theme';
+
+$_lang['fred.rebuild.rebuild'] = 'Rebuild';
+$_lang['fred.rebuild.rebuild_desc'] = 'To publish changes of updated Elements to existing pages, click the Rebuild button below.';
+
+$_lang['fred.element_cateogries.all'] = 'All Categories';
+$_lang['fred.element_categories.none'] = 'No categories were found.';
+$_lang['fred.element_categories.name'] = 'Name';
+$_lang['fred.element_categories.number_of_elements'] = '# Elements';
+$_lang['fred.element_categories.rank'] = 'Rank';
+$_lang['fred.element_categories.search_name'] = 'Search by Name';
+$_lang['fred.element_categories.create'] = 'Create Category';
+$_lang['fred.element_categories.update'] = 'Update Category';
+$_lang['fred.element_categories.remove'] = 'Remove Category';
+$_lang['fred.element_categories.remove_confirm'] = 'Are you sure you want to remove "[[+name]]" Element Category and all [[+elements]] attached Elements?' ;
+$_lang['fred.element_categories.remove_confirm_singular'] = 'Are you sure you want to remove "[[+name]]" Element Category and [[+elements]] attached Element?' ;
+$_lang['fred.element_categories.remove_confirm_empty'] = 'Are you sure you want to remove "[[+name]]" Element Category?' ;
+$_lang['fred.element_categories.duplicate'] = 'Duplicate Category';
+$_lang['fred.element_categories.new_name'] = 'New Name';
+$_lang['fred.element_categories.theme'] = 'Theme';
+
+$_lang['fred.element_option_sets.none'] = 'No option sets were found.';
+$_lang['fred.element_option_sets.name'] = 'Name';
+$_lang['fred.element_option_sets.description'] = 'Description';
+$_lang['fred.element_option_sets.complete'] = 'Complete';
+$_lang['fred.element_option_sets.search_name'] = 'Search by Name';
+$_lang['fred.element_option_sets.create'] = 'Create Option Set';
+$_lang['fred.element_option_sets.update'] = 'Update Option Set';
+$_lang['fred.element_option_sets.remove'] = 'Remove Option Set';
+$_lang['fred.element_option_sets.remove_confirm'] = 'Are you sure you want to delete [[+name]] Option Set?';
+$_lang['fred.element_option_sets.quick_update'] = 'Quick Update Option Set';
+$_lang['fred.element_option_sets.data'] = 'Data';
+$_lang['fred.element_option_sets.no_set'] = 'None';
+$_lang['fred.element_option_sets.duplicate'] = 'Duplicate Option Set';
+$_lang['fred.element_option_sets.new_name'] = 'New Name';
+$_lang['fred.element_option_sets.preview'] = 'Preview Option Set';
+$_lang['fred.element_option_sets.theme'] = 'Theme';
+
+$_lang['fred.element_rte_configs.none'] = 'No rte configs were found.';
+$_lang['fred.element_rte_configs.name'] = 'Name';
+$_lang['fred.element_rte_configs.description'] = 'Description';
+$_lang['fred.element_rte_configs.search_name'] = 'Search by Name';
+$_lang['fred.element_rte_configs.create'] = 'Create RTE Config';
+$_lang['fred.element_rte_configs.update'] = 'Update RTE Config';
+$_lang['fred.element_rte_configs.remove'] = 'Remove RTE Config';
+$_lang['fred.element_rte_configs.remove_confirm'] = 'Are you sure you want to delete [[+name]] RTE Config?';
+$_lang['fred.element_rte_configs.quick_update'] = 'Quick Update RTE Config';
+$_lang['fred.element_rte_configs.data'] = 'Data';
+$_lang['fred.element_rte_configs.duplicate'] = 'Duplicate RTE Config';
+$_lang['fred.element_rte_configs.new_name'] = 'New Name';
+$_lang['fred.element_rte_configs.theme'] = 'Theme';
+
+$_lang['fred.themes.none'] = 'No themes were found.';
+$_lang['fred.themes.name'] = 'Name';
+$_lang['fred.themes.description'] = 'Description';
+$_lang['fred.themes.create'] = 'Create Theme';
+$_lang['fred.themes.update'] = 'Update Theme';
+$_lang['fred.themes.remove'] = 'Remove Theme';
+$_lang['fred.themes.remove_confirm'] = 'Are you sure tou want to remove "[[+name]]" Theme?';
+$_lang['fred.themes.search_name'] = 'Search by Name';
+$_lang['fred.themes.new_name'] = 'New Name';
+$_lang['fred.themes.duplicate'] = 'Duplicate Theme';
+$_lang['fred.themes.all'] = 'All Themes';
+$_lang['fred.themes.version'] = 'Version';
+$_lang['fred.themes.build'] = 'Build Theme';
+$_lang['fred.themes.release'] = 'Release';
+$_lang['fred.themes.general'] = 'General';
+$_lang['fred.themes.categories'] = 'Included Element categories for required Snippets, Chunks and Plugins';
+$_lang['fred.themes.add_dependency'] = 'Add Extra';
+$_lang['fred.themes.version'] = 'Version';
+$_lang['fred.themes.package_name'] = 'Package Name';
+$_lang['fred.themes.remove_dependency'] = 'Remove Dependency';
+$_lang['fred.themes.changelog'] = 'Changelog';
+$_lang['fred.themes.license'] = 'License';
+$_lang['fred.themes.readme'] = 'Read Me';
+$_lang['fred.themes.dependencies'] = 'Extras';
+$_lang['fred.themes.docs'] = 'Docs';
+$_lang['fred.themes.latest_build'] = 'Latest Build';
+$_lang['fred.themes.download'] = 'Download Theme';
+$_lang['fred.themes.theme_folder'] = 'Theme Directory';
+$_lang['fred.themes.theme_dir_msg_title'] = 'Theme Directory Created';
+$_lang['fred.themes.theme_dir_msg'] = 'The directory for this theme is <strong>themes/[[+theme_folder]]/</strong> inside your assets directory (usually <strong>assets/themes/[[+theme_folder]]/</strong>).<br>The contents of this directory will be included if you build this theme for distribution, so please place all theme-related assets here.';
+$_lang['fred.themes.overview'] = 'Overview';
+$_lang['fred.themes.included_theme_folder'] = 'Assets directory for this Theme';
+$_lang['fred.themes.theme_build_desc'] = 'Build a complete package for the MODX Installer to share or submit to the Extras repository.';
+$_lang['fred.themes.theme_build_readme_desc'] = 'A good readme file should include important details about how your theme is intended to be used, where to submit bug reports and how to get support.';
+$_lang['fred.themes.theme_build_changelog_desc'] = 'List any new features and bugfixes since your initial release.';
+$_lang['fred.themes.theme_build_license_desc'] = 'For open source, a variety of options are available at <a href=” https://choosealicense.com/“>Choose a License</a> or <a href=“https://creativecommons.org/share-your-work/licensing-types-examples/licensing-examples/“>Creative Commons</a>. Note: commercial licenses for premium themes are OK, too!';
+$_lang['fred.themes.theme_build_dependencies_desc'] = 'Include any Extras required for this Theme to function fully.';
+$_lang['fred.themes.build_default_title'] = 'Can\'t build Default Theme';
+$_lang['fred.themes.build_default_desc'] = 'To protect against unintended changes to your site from upgrades, please create a clone of the Default theme before building.';
+$_lang['fred.themes.delete_theme_folder'] = 'Delete Theme Directory';
+$_lang['fred.themes.duplicate_theme_folder'] = 'Duplicate theme directory';
+$_lang['fred.themes.duplicate_theme_objects'] = 'Duplicate theme Objects (Elements, Blueprints, etc.)';
+$_lang['fred.themes.build_and_download'] = 'Build & Download Theme';
+$_lang['fred.themes.theme_duplicate_name'] = '[[+theme]] Copy';
+$_lang['fred.themes.duplicate_templates'] = 'Duplicate Templates';
+$_lang['fred.themes.default_element'] = 'Default Element';
+
+$_lang['fred.themed_templates.theme'] = 'Theme';
+$_lang['fred.themed_templates.template'] = 'Template';
+$_lang['fred.themed_templates.create'] = 'Assign Theme to a Template';
+$_lang['fred.themed_templates.update'] = 'Update Assigned Theme';
+$_lang['fred.themed_templates.remove'] = 'Unassign Theme';
+$_lang['fred.themed_templates.remove_confirm'] = 'Are you sure you want to unassign theme: [[+theme]] from a template: [[+template]]?';
+
+$_lang['fred.media_sources.name'] = 'Name';
+$_lang['fred.media_sources.description'] = 'Description';
+$_lang['fred.media_sources.update'] = 'Update Media Source';
+$_lang['fred.media_sources.fred'] = 'Enabled for Fred';
+$_lang['fred.media_sources.fred_read_only'] = 'Read Only for Fred';
+$_lang['fred.media_sources.search'] = 'Search';
+$_lang['fred.media_sources.none'] = 'No Media Sources were found.';
+$_lang['fred.media_sources.go_to'] = 'Manage Media Sources';
+
+$_lang['fred.tvs.dropzone.hide_input'] = 'Hide Field from Manager';
+
+$_lang['setting_fred.launcher_position'] = 'Position of Launcher';
+$_lang['setting_fred.launcher_position_desc'] = 'Available values: bottom_left, bottom_right, top_left, top_right';
+$_lang['setting_fred.element_group_sort'] = 'Element Groups Sort';
+$_lang['setting_fred.element_group_sort_desc'] = 'Sort element groups by name or rank';
+$_lang['setting_fred.element_sort'] = 'Element Sort';
+$_lang['setting_fred.element_sort_desc'] = 'Sort elements by name or rank';
+$_lang['setting_fred.icon_editor'] = 'Icon Editor';
+$_lang['setting_fred.icon_editor_desc'] = 'Editor to use for Icons';
+$_lang['setting_fred.image_editor'] = 'Image Editor';
+$_lang['setting_fred.image_editor_desc'] = 'Editor to use for Images';
+$_lang['setting_fred.rte'] = 'Rich Text Editor';
+$_lang['setting_fred.rte_desc'] = 'Editor to use for Rich Text areas';
+$_lang['setting_fred.default_element'] = 'Default Element';
+$_lang['setting_fred.default_element_desc'] = 'Set a default <b>element ID</b> and <b>target</b> for converting resources to Fred. E.g. 10|target';
+$_lang['setting_fred.blueprint_category_sort'] = 'Blueprint\'s Category Sort';
+$_lang['setting_fred.blueprint_category_sort_desc'] = 'Sort Blueprint Categories by name or rank';
+$_lang['setting_fred.blueprint_sort'] = 'Blueprint Sort';
+$_lang['setting_fred.blueprint_sort_desc'] = 'Sort Blueprints by name or rank';
+$_lang['setting_fred.secret'] = 'Secret';
+$_lang['setting_fred.secret_desc'] = 'Secret used in signing XHR requests';
+
+$_lang['fred.err.blueprint_categories_ns_name'] = 'Name is required';
+$_lang['fred.err.blueprints_ns_name'] = 'Name is required';
+$_lang['fred.err.blueprints_ns_category'] = 'Category is required';
+$_lang['fred.err.bad_sort_column'] = 'Sort grid by <strong>[[+column]]</strong> to use drag & drop sorting.';
+$_lang['fred.err.clear_filter'] = 'Clear filters to use drag & drop sorting.';
+$_lang['fred.err.required_filter'] = 'You have to filter by [[+filter]].';
+$_lang['fred.err.element_categories_ns_name'] = 'Name is required';
+$_lang['fred.err.element_categories_ns_theme'] = 'Theme is required';
+$_lang['fred.err.elements_ns_name'] = 'Name is required';
+$_lang['fred.err.elements_ns_category'] = 'Category is required';
+$_lang['fred.err.element_settings_ns_name'] = 'Name is required';
+$_lang['fred.err.element_settings_ae_name'] = 'Element Setting with this name already exists.';
+$_lang['fred.err.element_option_sets_ns_name'] = 'Name is required';
+$_lang['fred.err.element_option_sets_ns_theme'] = 'Theme is required';
+$_lang['fred.err.element_option_sets_ae_name'] = 'Option Set with this name already exists in the theme.';
+$_lang['fred.err.element_rte_configs_ns_name'] = 'Name is required';
+$_lang['fred.err.element_rte_configs_ns_theme'] = 'Theme is required';
+$_lang['fred.err.element_rte_configs_ae_name'] = 'RTE Config with this name already exists in the theme.';
+$_lang['fred.err.theme_ns_name'] = 'Name is required';
+$_lang['fred.err.blueprint_categories_ns_theme'] = 'Theme is required';
+$_lang['fred.err.themed_templates_ns_template'] = 'Template is required';
+$_lang['fred.err.themed_templates_ns_theme'] = 'Theme is required';
+$_lang['fred.err.themed_templates_ae_template'] = 'Theme is already assigned for this Template';
+$_lang['fred.err.fred_dependency_title'] = 'Required Dependency';
+$_lang['fred.err.fred_dependency_body'] = 'Fred dependency is required and can\'t be removed.';
+$_lang['fred.err.build_ns_name'] = 'Name is required.';
+$_lang['fred.err.build_ns_theme'] = 'Theme is required.';
+$_lang['fred.err.build_failed'] = 'Build failed.';
+$_lang['fred.err.theme_no_build'] = 'There\'s no build for the theme.';
+$_lang['fred.err.theme_name_invalid_format'] = 'Only lower case letters and underscore "_" are allowed.';
+$_lang['fred.err.theme_docs_changelog_ns'] = 'Changelog is required';
+$_lang['fred.err.theme_docs_readme_ns'] = 'Read Me is required';
+$_lang['fred.err.theme_docs_license_ns'] = 'License is required';
+$_lang['fred.err.theme_no_built_file'] = 'Transport package not found.';
+$_lang['fred.err.theme_read_file'] = 'Transport package is unreadable.';
+$_lang['fred.err.themes_version_number_format'] = 'Please specify version number in format X.Y.Z';
+$_lang['fred.err.themes_release_format'] = 'Invalid release. Only pl, alpha or beta with an optional index is allowed. Examples: pl; alpha3, beta5';
+$_lang['fred.err.blueprint_categories_ae_name'] = 'Blueprint Category with this name already exists.';
+$_lang['fred.err.blueprints_ae_name'] = 'Blueprint with this name already exists.';
+$_lang['fred.err.element_categories_ae_name'] = 'Element Category with this name already exists.';
+$_lang['fred.err.elements_ae_name'] = 'Element with this name already exists.';
+$_lang['fred.err.theme_ae_name'] = 'Theme with this name already exists.';
---
+++ core/components/fred/lexicon/nl/permissions.inc.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * This file is part of the Fred package.
+ *
+ * Copyright (c) MODX, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Permission English Lexicon Entries for Fred
+ *
+ * @package fred
+ * @subpackage lexicon
+ */
+
+$_lang['fred.permissions.fred'] = 'View Fred (mgr) (web)';
+$_lang['fred.permissions.fred_elements'] = 'View Elements (mgr) (web)';
+$_lang['fred.permissions.fred_element_save'] = 'Create/edit/duplicate Elements (mgr)';
+$_lang['fred.permissions.fred_element_delete'] = 'Delete Elements (mgr)';
+$_lang['fred.permissions.fred_element_screenshot'] = 'Take a screenshot to replace the current sidebar preview for Elements (web)';
+$_lang['fred.permissions.fred_element_move'] = 'Drag and drop Elements into drop zones (web)';
+$_lang['fred.permissions.fred_element_category_delete'] = 'Delete Element Categories (mgr)';
+$_lang['fred.permissions.fred_element_category_save'] = 'Create/edit/duplicate Element (mgr)';
+$_lang['fred.permissions.fred_element_categories'] = 'View Fred Element Categories (mgr)';
+$_lang['fred.permissions.fred_element_option_sets_save'] = 'Create/edit/duplicate Option Sets (mgr)';
+$_lang['fred.permissions.fred_element_option_sets_delete'] = 'Delete Option Sets (mgr)';
+$_lang['fred.permissions.fred_element_option_sets'] = 'View Option Sets (mgr)';
+$_lang['fred.permissions.fred_element_rte_config_save'] = 'Create/edit/duplicate RTE configs (mgr)';
+$_lang['fred.permissions.fred_element_rte_config_delete'] = 'Delete RTE configs (mgr)';
+$_lang['fred.permissions.fred_element_rtes'] = 'View RTE configs (mgr)';
+$_lang['fred.permissions.fred_element_rebuild'] = 'View the Rebuild tab (mgr)';
+$_lang['fred.permissions.fred_themes'] = 'View the Themes tab (mgr)';
+$_lang['fred.permissions.fred_themes_save'] = 'Create/edit/duplicate Themes (mgr)';
+$_lang['fred.permissions.fred_themes_delete'] = 'Delete Themes (mgr)';
+$_lang['fred.permissions.fred_themes_build'] = 'Build Themes (mgr)';
+$_lang['fred.permissions.fred_themed_templates_save'] = 'Assign/Update Fred Themes to MODX Templates (mgr)';
+$_lang['fred.permissions.fred_themed_templates_delete'] = 'Unassign Theme from MODX Templates (mgr)';
+$_lang['fred.permissions.fred_themed_templates'] = 'View Theme/Template assignments (mgr)';
+$_lang['fred.permissions.fred_settings'] = 'View the Settings menu in the sidebar (web)';
+$_lang['fred.permissions.fred_settings_tvs'] = 'View the TVs section under the Settings menu (web)';
+$_lang['fred.permissions.fred_settings_tags'] = 'View the Tags section under the Settings menu (web)';
+$_lang['fred.permissions.fred_settings_advanced'] = 'View the Advanced Settings under the Settings menu (web)';
+$_lang['fred.permissions.fred_media_sources'] = 'View Media Sources (mgr)';
+$_lang['fred.permissions.fred_blueprints'] = 'View Blueprints (mgr) (web)';
+$_lang['fred.permissions.fred_blueprints_save'] = 'Create/edit/duplicate Blueprints (mgr) (web)';
+$_lang['fred.permissions.fred_blueprints_delete'] = 'Delete Blueprints (mgr)';
+$_lang['fred.permissions.fred_blueprints_create_public'] = 'Create public Blueprints (mgr) (web)';
+$_lang['fred.permissions.fred_blueprint_categories'] = 'View Blueprint Categories (mgr)';
+$_lang['fred.permissions.fred_blueprint_categories_save'] = 'Create/edit/duplicate Blueprint Categories (mgr) (web)';
+$_lang['fred.permissions.fred_blueprint_categories_delete'] = 'Delete Blueprint Categories (mgr)';
+$_lang['fred.permissions.fred_blueprint_categories_create_public'] = 'Create Public Blueprint Categories (mgr) (web)';
+$_lang['fred.permissions.new_document_in_root'] = 'Create Resources in the top-level of web root (mgr) (web)';
+$_lang['fred.permissions.view_unpublished'] = 'View unpublished Resources (web)';
+$_lang['fred.permissions.resource_duplicate'] = 'Duplicate Resources (mgr) (web)';
+$_lang['fred.permissions.unpublish_document'] = 'Unpublish Resources (mgr) (web)';
+$_lang['fred.permissions.publish_document'] = 'Publish Resources (mgr) (web)';
+$_lang['fred.permissions.save_document'] = 'Save Resources (mgr) (web)';
+$_lang['fred.permissions.new_document'] = 'Create new Resources (mgr) (web)';
+$_lang['fred.permissions.delete_document'] = 'Delete Resources (mgr) (web)';
+$_lang['fred.permissions.undelete_document'] = 'Undelete Resources (mgr) (web)';
