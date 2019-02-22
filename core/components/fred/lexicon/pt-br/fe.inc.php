69ec5b782e37f4c0d919e09a03ed142a9cd9b814
John Peca <pecajan@gmail.com>
2019-02-22 17:43:14 +0100

    New translations fe.inc.php (Portuguese, Brazilian)

---
+++ core/components/fred/lexicon/pt-br/fe.inc.php
@@ -0,0 +1,164 @@
+<?php
+/*
+ * This file is part of the Fred package.
+ *
+ * Copyright (c) MODX, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Default Lexicon Entries for Fred's Frontend
+ *
+ * @package fred
+ * @subpackage lexicon
+ */
+
+$_lang['fred.fe.close'] = 'Close';
+$_lang['fred.fe.save'] = 'Save';
+$_lang['fred.fe.pages'] = 'Site';
+$_lang['fred.fe.elements'] = 'Elements';
+$_lang['fred.fe.page_settings'] = 'Settings';
+$_lang['fred.fe.more'] = 'More';
+$_lang['fred.fe.saving_page'] = 'Saving Page';
+$_lang['fred.fe.preparing_content'] = 'Preparing Content';
+$_lang['fred.fe.open_sidebar'] = 'Open Sidebar';
+$_lang['fred.fe.toggle_preview'] = 'Toggle Preview';
+$_lang['fred.fe.close_sidebar'] = 'Close Sidebar';
+$_lang['fred.fe.element_settings'] = 'Element Settings';
+$_lang['fred.fe.apply'] = 'Apply';
+$_lang['fred.fe.cancel'] = 'Cancel';
+$_lang['fred.fe.clear'] = 'Clear';
+$_lang['fred.fe.browse'] = 'Browse';
+$_lang['fred.fe.browse_files'] = 'Browse Files';
+$_lang['fred.fe.browse_images'] = 'Browse Images';
+$_lang['fred.fe.turn_off_fred'] = 'Disable Fred';
+$_lang['fred.fe.turn_on_fred'] = 'Enable Fred';
+$_lang['fred.fe.invalid_elements_warning'] = 'One or more Elements in the content do not belong to this Theme.';
+
+$_lang['fred.fe.page_settings.page_title'] = 'Page Title';
+$_lang['fred.fe.page_settings.long_title'] = 'Long Title';
+$_lang['fred.fe.page_settings.description'] = 'Description';
+$_lang['fred.fe.page_settings.intro_text'] = 'Intro Text';
+$_lang['fred.fe.page_settings.menu_title'] = 'Menu Title';
+$_lang['fred.fe.page_settings.alias'] = 'Alias';
+$_lang['fred.fe.page_settings.published'] = 'Published';
+$_lang['fred.fe.page_settings.hide_from_menu'] = 'Hide from Menu';
+$_lang['fred.fe.page_settings.advanced_settings'] = 'Advanced Settings';
+$_lang['fred.fe.page_settings.published_on'] = 'Published On';
+$_lang['fred.fe.page_settings.publish_on'] = 'Publish On';
+$_lang['fred.fe.page_settings.unpublish_on'] = 'Unpublish On';
+$_lang['fred.fe.page_settings.menu_index'] = 'Menu Index';
+$_lang['fred.fe.page_settings.deleted'] = 'Deleted';
+$_lang['fred.fe.page_settings.tvs'] = 'TVs';
+
+$_lang['fred.fe.pages.no_parent'] = 'No Parent';
+$_lang['fred.fe.pages.create_page'] = 'Create Page';
+$_lang['fred.fe.pages.parent'] = 'Parent';
+$_lang['fred.fe.pages.template'] = 'Template';
+$_lang['fred.fe.pages.page_title'] = 'Page Title';
+$_lang['fred.fe.pages.creating_page'] = 'Creating Page';
+$_lang['fred.fe.pages.expand_page'] = 'Expand Page';
+$_lang['fred.fe.pages.collapse_page'] = 'Collapse Page';
+$_lang['fred.fe.pages.edit'] = 'Edit';
+$_lang['fred.fe.pages.duplicate'] = 'Duplicate';
+$_lang['fred.fe.pages.publish'] = 'Publish';
+$_lang['fred.fe.pages.unpublish'] = 'Unpublish';
+$_lang['fred.fe.pages.create_child_page'] = 'Create Child Page';
+$_lang['fred.fe.pages.delete'] = 'Delete';
+$_lang['fred.fe.pages.undelete'] = 'Undelete';
+$_lang['fred.fe.pages.blueprint'] = 'Blueprint';
+$_lang['fred.fe.pages.publishing_page'] = 'Publishing Page';
+$_lang['fred.fe.pages.unpublishing_page'] = 'Unpublishing Page';
+$_lang['fred.fe.pages.deleting_page'] = 'Deleting Page';
+$_lang['fred.fe.pages.undeleting_page'] = 'Undeleting Page';
+$_lang['fred.fe.pages.deleted'] = 'Page was deleted';
+$_lang['fred.fe.pages.undeleted'] = 'Page was undeleted';
+$_lang['fred.fe.pages.duplicated'] = 'Page was duplicated';
+$_lang['fred.fe.pages.published'] = 'Page was published';
+$_lang['fred.fe.pages.unpublished'] = 'Page was unpublished';
+$_lang['fred.fe.pages.created'] = 'Page was created';
+$_lang['fred.fe.pages.updated'] = 'Page was updated';
+
+$_lang['fred.fe.more.openmanager'] = 'View in Manager';
+$_lang['fred.fe.more.logout'] = 'Logout';
+
+$_lang['fred.fe.blueprints'] = 'Blueprints';
+$_lang['fred.fe.blueprints.create_category'] = 'Create Category';
+$_lang['fred.fe.blueprints.category_name'] = 'Name';
+$_lang['fred.fe.blueprints.creating_blueprint_category'] = 'Creating Blueprint Category';
+$_lang['fred.fe.blueprints.create_blueprint'] = 'Create Blueprint';
+$_lang['fred.fe.blueprints.creating_blueprint'] = 'Creating Blueprint';
+$_lang['fred.fe.blueprints.category_public'] = 'Public';
+$_lang['fred.fe.blueprints.category_rank'] = 'Rank';
+
+$_lang['fred.fe.blueprints.blueprint_name'] = 'Name';
+$_lang['fred.fe.blueprints.blueprint_description'] = 'Description';
+$_lang['fred.fe.blueprints.blueprint_category'] = 'Category';
+$_lang['fred.fe.blueprints.blueprint_rank'] = 'Rank';
+$_lang['fred.fe.blueprints.blueprint_public'] = 'Public';
+$_lang['fred.fe.blueprints.building_content_from_blueprint'] = 'Building Content From Blueprint';
+$_lang['fred.fe.blueprints.blueprint_image'] = 'Image';
+
+$_lang['fred.fe.content.duplicate'] = 'Duplicate';
+$_lang['fred.fe.content.delete'] = 'Delete';
+$_lang['fred.fe.content.settings'] = 'Open Settings';
+$_lang['fred.fe.content.move_up'] = 'Move Up';
+$_lang['fred.fe.content.move_down'] = 'Move Down';
+$_lang['fred.fe.content.partial_blueprint'] = 'Create Partial Blueprint';
+$_lang['fred.fe.content.element_screenshot'] = 'Element Screenshot';
+$_lang['fred.fe.content.replace_element_thumbnail'] = 'Replace Element Thumbnail';
+$_lang['fred.fe.content.element_replacing_thumbnail'] = 'Replacing Element\'s Thumbnail';
+
+$_lang['fred.fe.element_settings.unsaved_changes'] = 'There are unsaved changes, are you sure?';
+
+$_lang['fred.fe.editor.edit'] = 'Edit';
+$_lang['fred.fe.editor.edit_icon'] = 'Edit Icon';
+$_lang['fred.fe.editor.edit_image'] = 'Edit Image';
+$_lang['fred.fe.editor.image_uri'] = 'Image URI';
+
+$_lang['fred.fe.preview.tablet_portrait'] = 'Tablet Portrait';
+$_lang['fred.fe.preview.tablet_landscape'] = 'Tablet Landscape';
+$_lang['fred.fe.preview.phone_portrait'] = 'Phone Portrait';
+$_lang['fred.fe.preview.phone_landscape'] = 'Phone Landscape';
+$_lang['fred.fe.preview.auto'] = 'Auto';
+
+$_lang['fred.fe.tagger.tagger'] = 'Tags';
+$_lang['fred.fe.tagger.add'] = 'Add Tag';
+$_lang['fred.fe.tagger.toggle_list'] = 'Toggle List';
+$_lang['fred.fe.tagger.toggle_input'] = 'Toggle Input';
+
+$_lang['fred.fe.permission.save'] = 'Insufficient permissions to save this resource!';
+$_lang['fred.fe.permission.new_document'] = 'Insufficient permissions to create a resource!';
+$_lang['fred.fe.permission.new_document_in_root'] = 'Insufficient permissions to create a resource at root!';
+
+$_lang['fred.fe.more.help'] = 'Help';
+
+$_lang['fred.fe.err.permission_denied'] = 'Permission denied.';
+$_lang['fred.fe.err.blueprints_ns_name'] = 'Name is required';
+$_lang['fred.fe.err.blueprints_ns_category'] = 'Category is required';
+$_lang['fred.fe.err.blueprints_ae_name'] = 'Blueprint with this name already exists.';
+$_lang['fred.fe.err.blueprints_save'] = 'Saving Blueprint failed.';
+$_lang['fred.fe.err.blueprint_categories_ns_name'] = 'Name is required';
+$_lang['fred.fe.err.blueprint_categories_ns_theme'] = 'Theme is required';
+$_lang['fred.fe.err.blueprint_categories_ae_name'] = 'Blueprint Category with this name already exists.';
+$_lang['fred.fe.err.resource_ns_parent'] = 'Parent is required';
+$_lang['fred.fe.err.resource_ns_template'] = 'Template is required';
+$_lang['fred.fe.err.resource_ns_pagetitle'] = 'Page Title is required';
+$_lang['fred.fe.err.resource_save_new'] = 'Error creating new resource';
+$_lang['fred.fe.err.resource_ns_id'] = 'Resource is required';
+$_lang['fred.fe.err.resource_nf'] = 'Resource not found';
+$_lang['fred.fe.err.resource_duplicate_failed'] = 'Duplication failed';
+$_lang['fred.fe.err.elements_ns_element'] = 'Element is required';
+$_lang['fred.fe.err.elements_ns_image'] = 'Image is required';
+$_lang['fred.fe.err.elements_ns_category'] = 'Category is required';
+$_lang['fred.fe.err.category_no_theme'] = 'Category does not have a Theme.';
+$_lang['fred.fe.err.elements_save'] = 'Saving Element failed.';
+$_lang['fred.fe.err.resource_invalid_id'] = 'Invalid id was provided';
+$_lang['fred.fe.err.resource_nf_id'] = 'Could not load resource with id [[+id]]';
+$_lang['fred.fe.err.resource_ns_element'] = 'Element is required';
+$_lang['fred.fe.err.resource_ns_data'] = 'Data is required';
+$_lang['fred.fe.err.resource_stale'] = 'Your page is outdated, please reload the page.';
+$_lang['fred.fe.err.resource_save'] = 'Error saving resource';
+$_lang['fred.fe.err.tagger_ns_group'] = 'Group is required';
