0a6125d2e5b76730e535f9556320451af4f34013
Jan Peca <pecajan@gmail.com>
2019-05-07 14:17:14 +0200

    Fix creting themed template with a default blueprint

--- core/components/fred/processors/mgr/themed_templates/create.class.php
+++ core/components/fred/processors/mgr/themed_templates/create.class.php
@@ -18,11 +18,12 @@ class FredThemedTemplatesCreateProcessor extends modProcessor
 
         return parent::initialize();
     }
-    
+
     public function process()
     {
         $templates = $this->getProperty('templates');
         $theme = $this->getProperty('theme');
+        $defaultBlueprint = (int)$this->getProperty('default_blueprint', 0);
 
         if (empty($theme)) {
             $this->addFieldError('theme', $this->modx->lexicon('fred.err.themed_templates_ns_theme'));
@@ -31,22 +32,23 @@ class FredThemedTemplatesCreateProcessor extends modProcessor
         if (empty($templates)) {
             $this->addFieldError('templates', $this->modx->lexicon('fred.err.themed_templates_ns_template'));
         }
-        
+
         if ($this->hasErrors()) {
             return $this->failure();
         }
-        
+
         foreach ($templates as $template) {
             $template = intval($template);
-            
+
             if (empty($template)) continue;
-            
+
             $exists = $this->modx->getCount($this->classKey, ['template' => $template]);
             if ($exists > 0) continue;
-            
+
             $themedTemplate = $this->modx->newObject($this->classKey);
             $themedTemplate->set('template', $template);
             $themedTemplate->set('theme', $theme);
+            $themedTemplate->set('default_blueprint', $defaultBlueprint);
             $themedTemplate->save();
         }
 
