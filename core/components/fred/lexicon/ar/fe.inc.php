59aa194702ce1aa0c07752f3cbe801e8396acdfa
John Peca <pecajan@gmail.com>
2019-06-03 17:20:19 +0200

    New translations fe.inc.php (Arabic)

--- core/components/fred/lexicon/ar/fe.inc.php
+++ core/components/fred/lexicon/ar/fe.inc.php
@@ -87,9 +87,9 @@ $_lang['fred.fe.more.openmanager'] = 'View in Manager';
 $_lang['fred.fe.more.logout'] = 'Logout';
 
 $_lang['fred.fe.blueprints'] = 'Blueprints';
-$_lang['fred.fe.blueprints.create_category'] = 'Create Category';
+$_lang['fred.fe.blueprints.create_category'] = 'New Category';
 $_lang['fred.fe.blueprints.category_name'] = 'Name';
-$_lang['fred.fe.blueprints.creating_blueprint_category'] = 'Creating Blueprint Category';
+$_lang['fred.fe.blueprints.creating_blueprint_category'] = 'Adding Blueprint Category';
 $_lang['fred.fe.blueprints.create_blueprint'] = 'Create Blueprint';
 $_lang['fred.fe.blueprints.creating_blueprint'] = 'Creating Blueprint';
 $_lang['fred.fe.blueprints.category_public'] = 'Public';
