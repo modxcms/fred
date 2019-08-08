100fcfd357a89c95854711829638e719d61468c0
Jan Peca <pecajan@gmail.com>
2019-08-08 15:58:01 +0200

    Allow searching resource by ID

--- core/components/fred/model/fred/src/Endpoint/Ajax/GetResources.php
+++ core/components/fred/model/fred/src/Endpoint/Ajax/GetResources.php
@@ -26,19 +26,19 @@ class GetResources extends Endpoint
     {
         $context = $this->getClaim('context');
         $context = !empty($context) ? $context : 'web';
-        
+
         $query = $_GET['query'];
         $current = isset($_GET['current']) ? intval($_GET['current']) : 0;
         $parents = isset($_GET['parents']) ? $_GET['parents'] : '';
         $resources = isset($_GET['resources']) ? $_GET['resources'] : '';
         $depth = isset($_GET['depth']) ? intval($_GET['depth']) : 1;
         $limit = isset($_GET['limit']) ? intval($_GET['limit']) : 25;
-        
+
         $parents = Utils::explodeAndClean($parents, ',', 'intval', 0, 'strlen');
         $resources = Utils::explodeAndClean($resources, ',', 'intval');
-        
+
         $currentResource = null;
-        
+
         if (!empty($current)) {
             $currentResource = $this->modx->getObject('modResource', $current);
             if ($currentResource) {
@@ -54,25 +54,25 @@ class GetResources extends Endpoint
                 $currentResource = null;
             }
         }
-        
+
         $c = $this->modx->newQuery('modResource');
         $where = [
             'context_key' => $context
         ];
-        
+
         if (!empty($current)) {
             $where['id:!='] = $current;
         }
-        
+
         if (!empty($parents) || !empty($resources)) {
             $resourceIDs = [];
-            
+
             if (!empty($resources)) {
                 $resourceIDs = $resources;
             } else {
                 foreach ($parents as $parent) {
                     $resourceIDs[] = $parent;
-                    
+
                     $childIDs = $this->modx->getChildIds($parent, $depth, ['context' => $context]);
                     if (!empty($childIDs)) {
                         $resourceIDs = array_merge($resourceIDs, $childIDs);
@@ -84,21 +84,22 @@ class GetResources extends Endpoint
 
             $where['id:IN'] = $resourceIDs;
         }
-        
+
         $c->limit($limit);
         $c->sortby('menuindex', 'ASC');
-        
+
         if (!empty($query)) {
             $where['pagetitle:LIKE'] = '%' . $query . '%';
+            $where['OR:id:='] = intval($query);
         }
-        
+
         $c->where($where);
 
         $data = [];
-        
+
         /** @var \modResource[] $resourcesIterator */
         $resourcesIterator = $this->modx->getIterator('modResource', $c);
-        
+
         foreach ($resourcesIterator as $resource) {
             if (!$resource->checkPolicy('list')) continue;
             $data[] = [
