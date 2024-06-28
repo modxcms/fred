a86415bc449edfff3cfe80d9c7faaafb8a1beebb
matdave <mat@matdave.com>
2024-06-28 10:42:14 -0500

    fixing issue #504

--- core/components/fred/src/Elements/Event/OnDocFormSave.php
+++ core/components/fred/src/Elements/Event/OnDocFormSave.php
@@ -5,4 +5,6 @@ namespace Fred\Elements\Event;
 class OnDocFormSave extends Event
 {
     use \Fred\Traits\Elements\Event\OnDocFormSave;
+
+    private $renderResource = \Fred\RenderResource::class;
 }
