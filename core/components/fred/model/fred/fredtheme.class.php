7e558b47f53f1253f16b56669c67e60865be8d94
matdave <mat@matdave.com>
2024-10-16 10:48:55 -0500

    add theme settings model for 2.x

--- core/components/fred/model/fred/fredtheme.class.php
+++ core/components/fred/model/fred/fredtheme.class.php
@@ -22,7 +22,7 @@ class FredTheme extends xPDOSimpleObject
             $this->_fields['namespace'] = $namespaceName;
             $this->setDirty('namespace');
 
-            /** @var modNamespace $namespace */
+            /** @var $namespace */
             $namespace = $this->xpdo->getObject('modNamespace', ['name' => $namespaceName]);
             if (!$namespace) {
                 $namespace = $this->xpdo->newObject('modNamespace');
@@ -62,7 +62,7 @@ class FredTheme extends xPDOSimpleObject
         $themeDirLexicon->set('value', 'Theme Directory');
         $themeDirLexicon->save();
 
-        /** @var modLexiconEntry $themeDirDescLexicon */
+        /** @var $themeDirDescLexicon */
         $themeDirDescLexicon = $this->xpdo->getObject('modLexiconEntry', [
             'namespace' => $this->namespace,
             'name' => "setting_{$this->settingsPrefix}.theme_dir_desc"
@@ -194,5 +194,386 @@ class FredTheme extends xPDOSimpleObject
         $uri = rtrim($uri, '/') . '/';
 
         return $uri;
+    }
+
+    public function getThemeSettingKey($key)
+    {
+        return "$this->settingsPrefix.setting.$key";
+    }
+
+    public function getThemeSettingXType($setting)
+    {
+        if (!empty($setting['xtype'])) return $setting['xtype'];
+
+        switch ($setting['type']) {
+            case 'slider':
+                return 'numberfield';
+            case 'toggle':
+                return 'combo-boolean';
+            case 'textarea':
+                return 'textarea';
+            case 'text':
+            case 'page':
+            case 'chunk':
+            case 'file':
+            case 'folder':
+            case 'image':
+            case 'tagger':
+            case 'togglegroup':
+            case 'colorswatch':
+            case 'colorpicker':
+            case 'select':
+            default:
+                return 'textfield';
+        }
+    }
+
+    private function syncThemeSettings()
+    {
+        $settings = $this->get('settings');
+        if (empty($settings) || !is_array($settings)) {
+            $this->xpdo->removeCollection('modSystemSetting', ['namespace' => $this->namespace, 'key:LIKE' => "$this->settingsPrefix.setting.%"]);
+            $this->xpdo->removeCollection('modContextSetting', ['namespace' => $this->namespace, 'key:LIKE' => "$this->settingsPrefix.setting.%"]);
+            return;
+        }
+
+        $touchedSettings = [];
+
+        foreach ($settings as $setting) {
+            if (!empty($setting['group'])) {
+                if (empty($setting['settings'])) {
+                    continue;
+                }
+
+                foreach ($setting['settings'] as $groupSetting) {
+                    $touchedSettings[] = $this->getThemeSettingKey($groupSetting['name']);
+                    $groupSetting['xtype'] = $this->getThemeSettingXType($groupSetting);
+                    $this->syncThemeSetting($groupSetting, $setting['group']);
+                }
+                continue;
+            }
+
+            $setting['xtype'] = $this->getThemeSettingXType($setting);
+            $touchedSettings[] = $this->getThemeSettingKey($setting['name']);
+            $this->syncThemeSetting($setting);
+        }
+
+        $where = [
+            'namespace' => $this->namespace,
+            'key:LIKE' => "$this->settingsPrefix.setting.%",
+        ];
+
+        if (!empty($touchedSettings)) {
+            $where['key:NOT IN'] = $touchedSettings;
+        }
+
+        $this->xpdo->removeCollection('modSystemSetting', $where);
+        $this->xpdo->removeCollection('modContextSetting', $where);
+    }
+
+    private function syncThemeSetting($setting, $group = '')
+    {
+        $systemSetting = $this->xpdo->getObject('modSystemSetting', ['namespace' => $this->namespace, 'key' => $this->getThemeSettingKey($setting['name'])]);
+        if (!$systemSetting) {
+            $systemSetting = $this->xpdo->newObject('modSystemSetting');
+            $systemSetting->set('namespace', $this->namespace);
+            $systemSetting->set('key', $this->getThemeSettingKey($setting['name']));
+
+            $systemSetting->set('value', $this->themeSettingValueToModxPlaceholder($setting['value']));
+        }
+
+        $systemSetting->set('xtype', $setting['xtype']);
+        $systemSetting->set('area', $group);
+        $systemSetting->save();
+    }
+
+    public function saveThemeSettings($settingValues)
+    {
+        $settings = $this->get('settings');
+        if (empty($settings)) {
+            return;
+        }
+
+        /** @var \modX $modx */
+        $modx = $this->xpdo;
+
+        if (!$modx->user->get('sudo')) {
+            $settings = $this->filterThemeSettings($settings, false, false);
+        }
+
+        $keys = [];
+
+        foreach ($settings as $setting) {
+            if (isset($setting['group']) && is_array($setting['settings'])) {
+                foreach ($setting['settings'] as $groupSetting) {
+                    $keys[] = $groupSetting['name'];
+                }
+                continue;
+            }
+
+            $keys[] = $setting['name'];
+        }
+
+        $keys = array_flip($keys);
+
+        foreach ($settingValues as $name => $value) {
+            if (!isset($keys[$name])) {
+                continue;
+            }
+
+            $setting = $this->xpdo->getObject('modSystemSetting', ['namespace' => $this->namespace, 'key' => $this->getThemeSettingKey($name)]);
+            if (!$setting) {
+                continue;
+            }
+
+            $setting->set('value', $this->themeSettingValueToModxPlaceholder($value));
+            $setting->save();
+        }
+
+        $this->reloadSystemSettings();
+    }
+
+    protected function reloadSystemSettings()
+    {
+        /** @var \modX $modx */
+        $modx = $this->xpdo;
+        $modx->getCacheManager();
+        $modx->cacheManager->refresh();
+
+        $config = $modx->cacheManager->get('config', [
+            \xPDO::OPT_CACHE_KEY => $modx->getOption('cache_system_settings_key', null, 'system_settings'),
+            \xPDO::OPT_CACHE_HANDLER => $modx->getOption('cache_system_settings_handler', null, $modx->getOption(\xPDO::OPT_CACHE_HANDLER)),
+            \xPDO::OPT_CACHE_FORMAT => (integer) $modx->getOption('cache_system_settings_format', null, $modx->getOption(\xPDO::OPT_CACHE_FORMAT, null, \xPDOCacheManager::CACHE_PHP)),
+        ]);
+
+        if (empty($config)) {
+            $config = $modx->cacheManager->generateConfig();
+        }
+
+        if (empty($config)) {
+            $config = [];
+            if (!$settings = $modx->getCollection('modSystemSetting')) {
+                return;
+            }
+            /** @var $setting */
+            foreach ($settings as $setting) {
+                $config[$setting->get('key')]= $setting->get('value');
+            }
+        }
+
+        $modx->config = array_merge($modx->config, $config);
+        $modx->_systemConfig = $modx->config;
+    }
+
+    public function themeSettingValueToModxPlaceholder($value)
+    {
+        $value = str_replace('{{theme_dir}}', "[[++{$this->settingsPrefix}.theme_dir]]", $value);
+
+        return $value;
+    }
+
+    public function themeSettingValueFromModxPlaceholder($value)
+    {
+        $value = str_replace("[[++{$this->settingsPrefix}.theme_dir]]", '{{theme_dir}}', $value);
+
+        return $value;
+    }
+
+    public function getAllSettingValues($withModxTags = false)
+    {
+        $settings = $this->get('settings');
+        if (empty($settings)) {
+            return [];
+        }
+
+        $output = [];
+
+        foreach ($settings as $setting) {
+            if (isset($setting['group']) && !empty($setting['settings'])) {
+                foreach ($setting['settings'] as $gSetting) {
+                    $output[$gSetting['name']] = $this->xpdo->getOption("$this->settingsPrefix.setting.{$gSetting['name']}");
+
+                    if (!$withModxTags) {
+                        $output[$gSetting['name']] = $this->themeSettingValueFromModxPlaceholder($output[$gSetting['name']]);
+                    }
+                }
+                continue;
+            }
+
+            $output[$setting['name']] = $this->xpdo->getOption("$this->settingsPrefix.setting.{$setting['name']}");
+            if (!$withModxTags) {
+                $output[$setting['name']] = $this->themeSettingValueFromModxPlaceholder($output[$setting['name']]);
+            }
+        }
+
+        return $output;
+    }
+
+    public function getSettings()
+    {
+        $settings = $this->get('settings');
+        if (empty($settings)) {
+            return [];
+        }
+
+        foreach ($settings as $key => $setting) {
+            if (isset($setting['group']) && !empty($setting['settings'])) {
+                foreach ($setting['settings'] as $gKey => $gSetting) {
+                    $settings[$key]['settings'][$gKey]['value'] = $this->xpdo->getOption("$this->settingsPrefix.setting.{$gSetting['name']}");
+                }
+                continue;
+            }
+
+            $settings[$key]['value'] = $this->xpdo->getOption("$this->settingsPrefix.setting.{$setting['name']}");
+        }
+
+        /** @var \modX $modx */
+        $modx = $this->xpdo;
+
+        if (!$modx->user->get('sudo')) {
+            $settings = $this->filterThemeSettings($settings, false, false);
+        }
+
+        return $settings;
+    }
+
+    public function getSettingKeys()
+    {
+        $settings = $this->get('settings');
+        if (empty($settings)) {
+            return [];
+        }
+
+        $keys = [];
+
+        foreach ($settings as $setting) {
+            if (isset($setting['group']) && is_array($setting['settings'])) {
+                foreach ($setting['settings'] as $groupSetting) {
+                    $keys[] = $groupSetting['name'];
+                }
+                continue;
+            }
+
+            $keys[] = $setting['name'];
+        }
+
+        return $keys;
+    }
+
+    private function filterThemeSettings($settings, $memberships, $rolesMap)
+    {
+        if ($memberships === false && $rolesMap === false) {
+            /** @var \modX $modx */
+            $modx = $this->xpdo;
+
+            $memberships = [];
+            $groups = $modx->user->getUserGroups();
+            $roles = [];
+
+            if (!empty($groups)) {
+                /** @var $memberGroups */
+                $memberGroups = $modx->getIterator('modUserGroupMember', ['user_group:IN' => $groups, 'member' => $modx->user->id]);
+                foreach ($memberGroups as $memberGroup) {
+                    $group = $memberGroup->getOne('UserGroup');
+                    if (!$group) {
+                        continue;
+                    }
+
+                    if (!isset($roles[$memberGroup->get('role')])) {
+                        $role = $memberGroup->getOne('UserGroupRole');
+                        if (!$role) {
+                            continue;
+                        }
+
+                        $roles[$memberGroup->get('role')] = $role->get('authority');
+                    }
+
+                    $memberships[$group->get('name')] = $roles[$memberGroup->get('role')];
+                }
+            }
+
+            $rolesMap = [];
+            /** @var $userGroupRoles */
+            $userGroupRoles = $modx->getIterator('modUserGroupRole');
+            foreach ($userGroupRoles as $userGroupRole) {
+                $rolesMap[$userGroupRole->get('name')] = $userGroupRole->get('authority');
+            }
+        }
+
+        $filtered = [];
+
+        foreach ($settings as $setting) {
+            $matchAll = (isset($setting['userGroupMatchAll'])) ? $setting['userGroupMatchAll'] : false;
+
+            if (isset($setting['userGroup']) && is_array($setting['userGroup'])) {
+                $match = false;
+
+                foreach ($setting['userGroup'] as $userGroup) {
+                    if (is_array($userGroup)) {
+                        if (!isset($memberships[$userGroup['group']])) {
+                            $match = false;
+
+                            if ($matchAll === true) {
+                                continue 2;
+                            } else {
+                                continue;
+                            }
+                        }
+
+                        if (isset($userGroup['role'])) {
+                            if (!isset($rolesMap[$userGroup['role']])) {
+                                continue 2;
+                            }
+
+                            if ($memberships[$userGroup['group']] <= $rolesMap[$userGroup['role']]) {
+                                $match = true;
+
+                                if ($matchAll === false) {
+                                    break;
+                                }
+                            } else {
+                                $match = false;
+
+                                if ($matchAll === true) {
+                                    continue 2;
+                                }
+                            }
+                        } else {
+                            $match = true;
+
+                            if ($matchAll === false) {
+                                break;
+                            }
+                        }
+                    } else {
+                        if (isset($memberships[$userGroup])) {
+                            $match = true;
+
+                            if ($matchAll === false) {
+                                break;
+                            }
+                        } else {
+                            $match = false;
+
+                            if ($matchAll === true) {
+                                continue 2;
+                            }
+                        }
+                    }
+                }
+
+                if ($match === false) {
+                    continue;
+                }
+            }
+
+            if (isset($setting['group']) && !empty($setting['settings'])) {
+                $setting['settings'] = $this->filterThemeSettings($setting['settings'], $memberships, $rolesMap);
+            }
+
+            $filtered[] = $setting;
+        }
+
+        return $filtered;
     }
 }
